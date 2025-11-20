4c71a87265aab04aa6d9568a84df477bffef6fc5
macd4083 <macd4083@stthomas.edu>
2025-11-20 10:03:06 -0600

    Create script.js
    now some java, slideshow startin!

---
+++ script.js
@@ -0,0 +1,32 @@
+const images = [
+  'images/photo1.jpg',
+  'images/photo2.jpg',
+  'images/photo3.jpg'
+];
+
+let current = 0;
+const imgEl = document.getElementById('main-image');
+
+function showImage(index) {
+  imgEl.src = images[index];
+}
+
+document.getElementById('prev').onclick = () => {
+  current = (current - 1 + images.length) % images.length;
+  showImage(current);
+};
+
+document.getElementById('next').onclick = () => {
+  current = (current + 1) % images.length;
+  showImage(current);
+};
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowLeft' || e.key === 'a') {
+    current = (current - 1 + images.length) % images.length;
+    showImage(current);
+  } else if (e.key === 'ArrowRight' || e.key === 'd') {
+    current = (current + 1) % images.length;
+    showImage(current);
+  }
+});
